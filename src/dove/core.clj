929c955524615d18d6e44497d2c8b0997b8ca39e
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2019-02-21 00:28:26 +0100

    New function f-keys
    Naively transform keys with function f. Good enough for proof of
    concept.

--- src/dove/core.clj
+++ src/dove/core.clj
@@ -23,12 +23,12 @@
   [^Schema record-schema]
   (to-spec! record-schema {}))
 
-(defn un-ns-kw-keys
+(defn f-keys
   "Recursively transforms all map keys from namespaced keywords to
   keywords without namespace."
-  [m]
-  (let [f (fn [[k v]] (if (keyword? k) [(keyword (name k)) v] [k v]))]
-    (walk/postwalk (fn [x] (if (map? x) (into {} (map f x)) x)) m)))
+  [f m]
+  (let [g (fn [[k v]] (if (keyword? k) [(f k) v] [k v]))]
+    (walk/postwalk (fn [x] (if (map? x) (into {} (map g x)) x)) m)))
 
 (infer-spec! (SomeAvroSchema/getClassSchema))
 
