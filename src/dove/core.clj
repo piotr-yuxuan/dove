9d879312e6cc898bf9af9048969e701ba98f52bc
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2019-02-21 01:46:16 +0100

    Clean core ns

--- src/dove/core.clj
+++ src/dove/core.clj
@@ -1,20 +1,10 @@
 (ns dove.core
   (:require [clojure.walk :as walk]
-            [clojure.spec.alpha :as s]
-            [clj-uuid :as uuid]
-            [clojure.spec.gen.alpha :as g]
-            [clojure.test.check.generators :as test.g]
-            [inet.data.ip :as ip])
-  (:import (org.apache.avro Schema)
-           (java.nio ByteBuffer)
-           (some.package SomeAvroSchema))
-  (:use dove.core-impl)
+            [dove.core-impl :as impl])
+  (:import (java.nio ByteBuffer))
   (:gen-class))
 
-(defn- bytes-to-uuid
-  [bytes]
-  (let [buffer ^ByteBuffer (ByteBuffer/wrap bytes)]
-    (uuid/v4 (.getLong buffer) (.getLong buffer))))
+(def to-spec! impl/to-spec!)
 
 
 
@@ -24,33 +14,3 @@
   [f m]
   (let [g (fn [[k v]] (if (keyword? k) [(f k) v] [k v]))]
     (walk/postwalk (fn [x] (if (map? x) (into {} (map g x)) x)) m)))
-
-(to-spec! (SomeAvroSchema/getClassSchema) {:dry-run? false})
-
-(s/def :some.package/UID
-  (s/with-gen
-    uuid?
-    #(test.g/fmap bytes-to-uuid (s/gen (->avro-fixed? 16)))))
-
-(->> :some.package/UID
-     s/gen
-     g/generate)
-
-(s/def :some.package/IPv4
-  (s/with-gen
-    ip/address?
-    #(test.g/fmap ip/address (s/gen (->avro-fixed? 4)))))
-
-(->> :some.package/IPv4
-     s/gen
-     g/generate)
-
-(s/def :some.package/IPv6
-  (s/with-gen
-    ip/address?
-    #(test.g/fmap ip/address (s/gen (->avro-fixed? 16)))))
-
-(->> :some.package/IPv6
-     s/gen
-     g/generate
-     (f-keys (comp keyword name)))
